a2d803ccb3fc604d50cec9a47c081a9b9b612200
Alex Oborin <cudlove.1@gmail.com>
2025-11-02 16:21:17 +0500

    interfaces part2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,20 +9,5 @@ import static org.example.HelperClass.print;
 public class Main {
     public static void main(String[] args) {
 
-        Client client = new Client("soup");
-        Client client2 = new Client("fish");
-        Client client3 = new Client("meat");
-        WaiterOne waiterOne = new WaiterOne();
-
-        client.takeOrder(new Waiter() {
-            @Override
-            public void bringOrder(String wish) {
-                print("this your`s " + wish);
-            }
-        }, client.wish);
-
-        client2.takeOrder(wish -> print("this your`s " + wish), client2.wish);
-
-        client3.takeOrder(waiterOne, client3.wish);
     }
 }
--- src/main/java/org/example/interfaces/Client.java
+++ src/main/java/org/example/interfaces/Client.java
@@ -1,5 +1,7 @@
 package org.example.interfaces;
 
+import static org.example.HelperClass.print;
+
 public class Client {
     public String wish;
 
@@ -8,7 +10,26 @@ public class Client {
     }
 
     public void takeOrder(Waiter waiter, String wish) {
-        this.wish = wish;
         waiter.bringOrder(wish);
         }
+
+        /// To main
+/*          Client client = new Client("soup");
+            Client client2 = new Client("fish");
+            Client client3 = new Client("meat");
+            WaiterOne waiterOne = new WaiterOne();
+
+        //АНОНИМНЫЙ КЛАСС
+        client.takeOrder(new Waiter() {
+        @Override
+        public void bringOrder(String wish) {
+            print("this your`s " + wish);
+        }
+    }, client.wish);
+
+        //ЛЯМБДА ВЫРАЖЕНИЕ
+        client2.takeOrder(wish -> print("this your`s " + wish), client2.wish);
+
+        //СТАРАЯ РЕАЛИЗАЦИЯ
+        client3.takeOrder(waiterOne, client3.wish);*/
 }
